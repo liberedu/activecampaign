8e7b7b3a2ec337ffbe3d935fc180bb2f77e0513a
AlexandreMPDias <alexandrempdias@icloud.com>
2022-10-31 18:12:45 -0300

    chore: add more contact-custom-fields

--- src/Resources/ContactCustomField.php
+++ src/Resources/ContactCustomField.php
@@ -5,25 +5,48 @@ namespace TestMonitor\ActiveCampaign\Resources;
 
 class ContactCustomField
 {
-     /**
-     * The id of the role field.
-     *
-     * @var string
-     */
-    public static $role_id = '1';
-     /**
-     * The id of the externalId field.
-     *
-     * @var string
-     */
-    public static string $externalId_id = '2';
+    public static $uuid_id = '25';
+    public static $role_id = '33' ;
+    public static $cpf_id = '5';
+    public static $picture_id = '6';
+    public static $birthday_id = '7';
 
     public static function role(?string $value = null) {
         return self::getField(self::$role_id, $value);
     }
 
-    public static function externalId(string $value) {
-        return self::getField(self::$externalId_id, $value);
+    public static function uuid(?string $value = null) {
+        return self::getField(self::$uuid_id, $value);
+    }
+
+    public static function cpf(?string $value = null) {
+        return self::getField(self::$cpf_id, $value);
+    }
+
+    public static function picture(?string $value = null) {
+        return self::getField(self::$picture_id, $value);
+    }
+
+    public static function birthday(?string $value = null) {
+        return self::getField(self::$birthday_id, $value);
+    }
+
+    public static function all(array $data): array {
+        $data = array_merge([
+            'role' => null,
+            'uuid' => null,
+            'cpf' => null,
+            'picture' => null,
+            'birthday' => null
+        ],$data);
+        $fields = [
+            self::role($data['role']),
+            self::uuid($data['uuid']),
+            self::birthday($data['birthday']),
+            self::picture($data['picture']),
+            self::cpf($data['cpf']),
+        ];
+        return array_filter($fields, fn (array $entry) => !empty($entry['value']));
     }
 
     private static function getField($id, $value): array {
